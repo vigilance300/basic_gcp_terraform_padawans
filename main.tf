74f03d522649f7226545f411bf7cc752a2e80eb8
unknown <vigilance300@gmail.com>
2024-04-23 21:59:39 -0400

    adding files to gcp terraform

---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.25.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+project = "theo-gcp-class-5"
+region = "us-central1"
+zone = "us-central1-a"
+credentials = "theo-gcp-class-5-17424e3ea23f.json"
+
+
+}
+
+resource "google_storage_bucket" "vigilance-bucket" {
+  name          = "vigilance-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
+
+
+resource "google_storage_bucket" "vigilance-lives" {
+  name          = "vigilance-lives"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
